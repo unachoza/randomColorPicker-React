b27248a6b8f81ede0ca0e59f07f6c727e9e9b439
Arianna Choza <unachoza@gmail.com>
2018-11-18 20:35:52 -0500

    Button and Random components displaying

---
+++ random-colors/src/components/Button.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Button extends React.Component {
+	render() {
+		return (
+			<button 
+				className={ this.props.light ? 'light-button' : 'dark-button' }>
+				Refresh
+			</button>
+		);
+	}
+}
+export default Button 
